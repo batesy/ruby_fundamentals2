d6a34f1fe94feb3eafd7c9cfc9905801af4072a2
batesy <gbatesy@gmail.com>
2013-10-22 17:16:51 -0400

    Checks to see if user needs to pick up bananas.

--- exercise6.rb
+++ exercise6.rb
@@ -3,3 +3,11 @@ grocery_list = ["eggs", "cheese", "bacon", "beer", "rice"]
 grocery_list.each {|i| puts "*#{i}"}
 
 puts "# of items on list: #{grocery_list.length}."
+puts
+
+if grocery_list.include?("bananas")
+	puts "You don't need to pick up bananas today."
+else
+	puts "You need to pick up bananas today."
+	
+end
