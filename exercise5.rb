94fe8b061107e6fadc8867d2b26cda5acf750823
batesy <gbatesy@gmail.com>
2013-10-22 16:54:25 -0400

    Takes F input, and outputs conversion to Celcious.

--- exercise5.rb
+++ exercise5.rb
@@ -1,4 +1,10 @@
 puts "Enter a temperature in Fahrenheit:"
-F = gets.chomp.to_i
+f = gets.chomp.to_i
 
-puts "The temperature you entered is: #{F}F."
+def convert(f)
+	(f - 32) * 5/9
+end
+
+c = convert(f)
+
+puts "The temperature you entered is: #{f}F, which is #{c}C."
